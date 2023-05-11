9de4b62e743bd7283d7b2cd2625f50fb05e92073
Ejiba Sandra Chisom <chisomejiba@gmail.com>
2023-05-11 09:24:06 -1200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+      const updateButton = document.querySelector("#updateButton");
+      const bioText = document.querySelector("#bio");
+
+      updateButton.addEventListener("click", () => {
+        bioText.textContent = "I am currently learning JavaScript";
+      });
+    
